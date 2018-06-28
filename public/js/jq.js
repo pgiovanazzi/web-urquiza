e20eab8951463198a390b38aab86e329a48d7986
Paulo <paulo.go@pm.me>
2018-06-28 04:53:33 -0300

    Cambios de estilo y fluides

--- public/js/jq.js
+++ public/js/jq.js
@@ -7,5 +7,11 @@ $(document).ready(function () {
   },
   {
     offset: '60%'
-  });
+	});
+
+	$('ul.nav li.dropdown').hover(function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+	});
 });
