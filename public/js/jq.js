b57e0d54b1efa2159ea772f76b7d6284bb6808bb
Paulo <paulo.go@pm.me>
2018-09-25 22:05:13 -0300

    Cambios frontend

--- public/js/jq.js
+++ public/js/jq.js
@@ -5,13 +5,27 @@ $(document).ready(function () {
   $contact.waypoint(function () {
     $contact.css("opacity", 1).addClass("bounceInLeft animated");
   },
-  {
-    offset: '60%'
-	});
+    {
+      offset: '60%'
+    });
 
-	$('ul.nav li.dropdown').hover(function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-	}, function() {
-		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-	});
+$(document).ready(function () {
+  $("#toggle-color-navbar").click(function () {
+    $(".navbar").toggleClass("color-onClick-menu");
+  });
+});
+
+$('ul.nav li.dropdown').hover(function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
+});
+
+$(window).scroll(function () {
+  if ($("nav").offset().top > 120) {
+    $("nav").addClass("navbar-after-scroll");
+  } else {
+    $("nav").removeClass("navbar-after-scroll");
+  }
 });
