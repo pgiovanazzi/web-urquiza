ed748bce23db668b7c055a15f50ba997a839c0dc
ivanog369 <ivanog@protonmail.com>
2018-12-13 23:53:16 -0300

    fixing

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,9 @@
 let mongoose = require('mongoose');
+
+// conectar al mlab
 mongoose.connect('mongodb://pi.automata:automatasdelsiglo21@ds011228.mlab.com:11228/urquizadb', { useNewUrlParser: true });
+
+//conectar al localhost
+//mongoose.connect('mongodb://localhost:27017/urquiza_db', { useNewUrlParser: true });
 
 module.exports = mongoose;
