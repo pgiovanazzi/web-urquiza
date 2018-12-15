5972b1feabdf66b049ce75bb4263bbd14369c133
ivanog369 <ivanog@protonmail.com>
2018-12-15 19:00:09 -0300

    change-login-size, db connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,7 @@
 let mongoose = require('mongoose');
+//mlab connection
 mongoose.connect('mongodb://pi.automata:automatasdelsiglo21@ds011228.mlab.com:11228/urquizadb', { useNewUrlParser: true });
 
+//localhost connection
+//mongoose.connect('mongodb://localhost:27017/urquiza_db', { useNewUrlParser: true });
 module.exports = mongoose;
