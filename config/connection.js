ddf28594ac427d5048a05d9ae0b1b41500dbdbb9
ivanog369 <ivanog@protonmail.com>
2019-03-23 03:53:52 -0300

    a change

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const mlab = 'mongodb://pi.automata:automatasdelsiglo21@ds011228.mlab.com:11228/
 //localhost
 const lh = 'mongodb://localhost:27017/urquiza_db'
 
-mongoose.connect(lh, { useNewUrlParser: true })
+mongoose.connect(mlab, { useNewUrlParser: true })
    .then(db => console.log("DB is connected"))
    .catch(err => console.error(err));
 
