978e8ab80fdaee47cad59db38d8acd10d67816d1
Paulo <paulo.go@pm.me>
2019-09-29 03:06:37 -0300

    text preinscription success added

--- client/src/components/PreInsSuccessComponent.vue
+++ client/src/components/PreInsSuccessComponent.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
     <section class="mb-4">
-      <div class="card text-center z-depth-2 p-4">
+      <div class="card text-center z-depth-2 p-5">
         <div class="card-body white-text">
           <div class="row">
             <div class="col-md-12">
-              <h3 class="mt-5 mb-4">Preincripción exitosa</h3>
+              <h3 class="mt-3 mb-4">Preincripción exitosa</h3>
             </div>
             <div class="col-md-12">
-              <span>Felicitaciónes! ya casi estas inscripto, solo necesitas acercarte a la institución y entregar algunos papeles. Nos vemos allá!!</span>
+              <span>Felicitaciónes! ya casi estas inscripto, solo necesitas acercarte a la institución y entregar las siguientes documentacines en los días hábiles de Lunes a Viernes de 20 a 22 hs en bedelía - 1º piso - Nivel Terciario Bv. Oroño 690</span>
+            </div>
+            <div class="col-md-12 mt-3 pt-3 border">
+              <ul>
+                <li>Fotocopia legalizada del DNI, CI, LE o LC.</li>
+                <li>Fotocopia legalizada del título secundario o título secundario en tramite.</li>
+                <li>Fotocopia legalizada de la partida de nacimiento.</li>
+                <li>Certificado de salud.</li>
+              </ul>
             </div>
           </div>
         </div>
